74b77028c4b0b634756840b6727ddb39ddc80d7d
burakmahim <burakmahim718@gmail.com>
2024-10-31 11:40:20 +0300

    Proje dosyası ekle.

---
+++ Constructor/Bebek.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Bebek
+    {
+        // Property tanımlamaları
+        public string _ad { get; set; }
+        public string _soyad { get; set; }
+        public DateTime _dogumTarihi { get; set; }
+
+        // Default constructor
+        public Bebek()
+        {
+            _dogumTarihi = DateTime.Now;
+            Console.WriteLine("Ingaaaa");
+        }
+
+        // Ad ve Soyad parametreleri alan constructor
+        public Bebek(string ad, string soyad)
+        {
+            _ad = ad;
+            _soyad = soyad;
+            _dogumTarihi = DateTime.Now;
+            Console.WriteLine("Ingaaaa");
+        }
+
+        // Bebek bilgilerini yazdıran metot
+        public void BebekBilgileriniYazdir()
+        {
+            Console.WriteLine($"Ad: {_ad}, Soyad: {_soyad}, Doğum Tarihi: {_dogumTarihi}");
+        }
+    }
+
+}
---
+++ Constructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Constructor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Default constructor ile bebek nesnesi oluşturma
+        Bebek bebek1 = new Bebek();
+        bebek1._ad = "Ali";
+        bebek1._soyad = "Kara";
+        bebek1.BebekBilgileriniYazdir();
+
+        Console.WriteLine();
+
+        // Parametreli constructor ile bebek nesnesi oluşturma
+        Bebek bebek2 = new Bebek("Ayşe", "Yılmaz");
+        bebek2.BebekBilgileriniYazdir();
+    }
+}
